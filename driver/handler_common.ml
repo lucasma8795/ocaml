48711126d31b92d9c590c14cd5792c97da5c29d3
Lucas Ma <51366352+lucasma8795@users.noreply.github.com>
2025-07-07 16:52:34 +0100

    Fix unintended effect being performed in the effect handler

--- driver/handler_common.ml
+++ driver/handler_common.ml
@@ -1,29 +1,28 @@
-open Effect.Deep
-open Load_path
-(* open Printf *)
-open Local_store
-
 module STbl = Misc.Stdlib.String.Tbl
+module Dir = Load_path.Dir
 
 type registry = string STbl.t
 
-let visible_files : registry ref = s_table STbl.create 42
-let visible_files_uncap : registry ref = s_table STbl.create 42
+let visible_files : registry ref = Local_store.s_table STbl.create 42
+let visible_files_uncap : registry ref = Local_store.s_table STbl.create 42
 
-let hidden_files : registry ref = s_table STbl.create 42
-let hidden_files_uncap : registry ref = s_table STbl.create 42
+let hidden_files : registry ref = Local_store.s_table STbl.create 42
+let hidden_files_uncap : registry ref = Local_store.s_table STbl.create 42
 
-let visible_dirs : Dir.t list ref = s_ref []
-let hidden_dirs  : Dir.t list ref = s_ref []
+let visible_dirs : Dir.t list ref = Local_store.s_ref []
+let hidden_dirs  : Dir.t list ref = Local_store.s_ref []
 
 let find_file_in_cache fn visible_files hidden_files =
-  try (STbl.find !visible_files fn, Visible) with
-  | Not_found -> (STbl.find !hidden_files fn, Hidden)
+  try (STbl.find !visible_files fn, Load_path.Visible) with
+  | Not_found -> (STbl.find !hidden_files fn, Load_path.Hidden)
 
 let is_basename fn = Filename.basename fn = fn
 
 let get_visible_path_list () = List.rev_map Dir.path !visible_dirs
 let get_hidden_path_list () = List.rev_map Dir.path !hidden_dirs
+
+let get_path_list () =
+  Misc.rev_map_end Dir.path !visible_dirs (List.rev_map Dir.path !hidden_dirs)
 
 let auto_include find_in_dir fn =
   if !Clflags.no_std_include then
@@ -47,14 +46,14 @@ let prepend_add dir =
     ) (Dir.files dir)
 
 let handle f =
-  match_with f ()
+  Effect.Deep.match_with f ()
   {
     retc = (fun ret -> ret);
     exnc = raise;
     effc = fun (type c) (eff: c Effect.t) ->
       match eff with
       (* find : string -> string *)
-      | Load_path fn ->
+      | Load_path.Load_path fn ->
         Some (fun (k: (c, _) continuation) ->
           let ret =
             try
@@ -65,10 +64,10 @@ let handle f =
             with Not_found ->
               auto_include Dir.find fn;
             in
-          continue k ret
+          Effect.Deep.continue k ret
         )
 
-      | Load_path_normalized fn ->
+      | Load_path.Load_path_normalized fn ->
         Some (fun (k: (c, _) continuation) ->
           match Misc.normalized_unit_filename fn with
           | Error _ -> raise Not_found
@@ -80,18 +79,18 @@ let handle f =
                   visible_files_uncap hidden_files_uncap
               else
                 try
-                  (Misc.find_in_path_normalized (get_visible_path_list ()) fn, Visible)
+                  (Misc.find_in_path_normalized (get_visible_path_list ()) fn, Load_path.Visible)
                 with
                 | Not_found ->
-                  (Misc.find_in_path_normalized (get_hidden_path_list ()) fn, Hidden)
+                  (Misc.find_in_path_normalized (get_hidden_path_list ()) fn, Load_path.Hidden)
             with Not_found ->
-              auto_include Dir.find_normalized fn_uncap, Visible
+              auto_include Dir.find_normalized fn_uncap, Load_path.Visible
             in
-          continue k ret
+          Effect.Deep.continue k ret
         )
 
       (* append_dir : Dir.t -> unit *)
-      | Append_dir dir ->
+      | Load_path.Append_dir dir ->
         Some (fun (k: (c, _) continuation) ->
           let update base fn visible_files hidden_files =
             if Dir.hidden dir && not (STbl.mem !hidden_files base) then
@@ -113,38 +112,38 @@ let handle f =
             hidden_dirs := dir :: !hidden_dirs
           else
             visible_dirs := dir :: !visible_dirs;
-          continue k ()
+          Effect.Deep.continue k ()
         )
 
       (* prepend_dir : Dir.t -> unit *)
-      | Prepend_dir dir ->
+      | Load_path.Prepend_dir dir ->
         Some (fun (k: (c, _) continuation) ->
           prepend_add dir;
           if Dir.hidden dir then
             hidden_dirs := !hidden_dirs @ [dir]
           else
             visible_dirs := !visible_dirs @ [dir];
-          continue k ()
+          Effect.Deep.continue k ()
         )
 
       (* remove_dir : Dir.t -> unit *)
-      | Remove_dir dir ->
+      | Load_path.Remove_dir dir ->
         Some (fun (k: (c, _) continuation) ->
           let visible = List.filter (fun d -> Dir.path d <> dir) !visible_dirs in
           let hidden = List.filter (fun d -> Dir.path d <> dir) !hidden_dirs in
           if   List.compare_lengths visible !visible_dirs <> 0
             || List.compare_lengths hidden !hidden_dirs <> 0 then begin
-            reset ();
+            Local_store.reset ();
             visible_dirs := visible;
             hidden_dirs := hidden;
             List.iter prepend_add hidden;
             List.iter prepend_add visible
           end;
-          continue k ()
+          Effect.Deep.continue k ()
         )
 
       (* reset : unit -> unit *)
-      | Reset_path ->
+      | Load_path.Reset_path ->
         Some (fun (k: (c, _) continuation) ->
           STbl.clear !hidden_files;
           STbl.clear !hidden_files_uncap;
@@ -152,42 +151,41 @@ let handle f =
           STbl.clear !visible_files_uncap;
           hidden_dirs := [];
           visible_dirs := [];
-          continue k ()
+          Effect.Deep.continue k ()
         )
 
       (* init : auto_include_callback -> string list -> string list -> unit
          assumes Reset_path has previously been performed *)
-      | Init_path (visible, hidden) ->
+      | Load_path.Init_path (visible, hidden) ->
         Some (fun (k: (c, _) continuation) ->
           visible_dirs := List.rev_map (Dir.create ~hidden:false) visible;
           hidden_dirs := List.rev_map (Dir.create ~hidden:true) hidden;
           List.iter prepend_add !hidden_dirs;
           List.iter prepend_add !visible_dirs;
-          continue k ()
+          Effect.Deep.continue k ()
         )
 
       (* get_visible : unit -> Dir.t list *)
-      | Get_visible ->
+      | Load_path.Get_visible ->
         Some (fun (k: (c, _) continuation) ->
-          continue k (List.rev !visible_dirs)
+          Effect.Deep.continue k (List.rev !visible_dirs)
         )
 
       (* get_path_list : unit -> string list *)
-      | Get_path_list ->
+      | Load_path.Get_path_list ->
         Some (fun (k: (c, _) continuation) ->
-          let ret = Misc.rev_map_end Dir.path !visible_dirs (List.rev_map Dir.path !hidden_dirs) in
-          continue k ret
+          Effect.Deep.continue k (get_path_list ())
         )
 
       (* get_paths : unit -> paths *)
-      | Get_paths ->
+      | Load_path.Get_paths ->
         Some (fun (k: (c, _) continuation) ->
-          let ret = {
+          let ret = Load_path.{
             visible = List.rev_map Dir.path !visible_dirs;
             hidden = List.rev_map Dir.path !hidden_dirs
           }
           in
-          continue k ret
+          Effect.Deep.continue k ret
         )
 
       | _ -> None
